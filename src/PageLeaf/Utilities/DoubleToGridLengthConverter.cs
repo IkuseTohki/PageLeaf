eee344c9c983ad295847efd6a782e1e02e67c873
IkuseTohki <four.seasons.haru@gmail.com>
2026-03-01 09:18:55 +0900

    docs(ui): コンバータークラスへのXMLドキュメンテーションコメントの追加
    WPFのバインディングで使用するユーティリティクラスに対し、XMLドキュメンテーションコメントを記述しました。各メソッドの役割、引数、戻り値を明文化することで、実装の意図を明確にし、開発時の保守性を向上させることを目的としています。
    
    - DoubleToGridLengthConverter.cs にクラス概要および Convert/ConvertBack メソッドのドキュメントを追加
    - MultiValueConverter.cs にクラス概要および Convert/ConvertBack メソッドのドキュメントを追加

--- src/PageLeaf/Utilities/DoubleToGridLengthConverter.cs
+++ src/PageLeaf/Utilities/DoubleToGridLengthConverter.cs
@@ -5,9 +5,21 @@ using System.Windows.Data;
 
 namespace PageLeaf.Utilities
 {
+    /// <summary>
+    /// double 型の値を WPF の GridLength 型に変換するコンバーターです。
+    /// 主に ViewModel の数値を Grid の幅や高さにバインドする際に使用します。
+    /// </summary>
     [ValueConversion(typeof(double), typeof(GridLength))]
     public class DoubleToGridLengthConverter : IValueConverter
     {
+        /// <summary>
+        /// double 値を GridLength に変換します。
+        /// </summary>
+        /// <param name="value">変換元の double 値。</param>
+        /// <param name="targetType">バインディング ターゲット プロパティの型。</param>
+        /// <param name="parameter">使用するコンバーター パラメーター。</param>
+        /// <param name="culture">コンバーターで使用するカルチャ。</param>
+        /// <returns>変換された GridLength オブジェクト。値が不正な場合は 0 の GridLength を返します。</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is double doubleValue)
@@ -22,6 +34,14 @@ namespace PageLeaf.Utilities
             return new GridLength(0);
         }
 
+        /// <summary>
+        /// GridLength を double 値に変換し戻します。
+        /// </summary>
+        /// <param name="value">変換元の GridLength オブジェクト。</param>
+        /// <param name="targetType">変換先の型。</param>
+        /// <param name="parameter">使用するコンバーター パラメーター。</param>
+        /// <param name="culture">コンバーターで使用するカルチャ。</param>
+        /// <returns>GridLength の数値部分。変換できない場合は 0.0 を返します。</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is GridLength gridLength)
